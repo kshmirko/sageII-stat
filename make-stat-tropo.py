5da20a54661f1016ae7fb1eeab278a35f342ee05
Dr. Konstantin Shmirko <shmirko.konstantin@gmail.com>
2014-03-26 19:10:12 +1100

    qwe

--- make-stat-tropo.py
+++ make-stat-tropo.py
@@ -28,54 +28,95 @@ def seasons(x):
 
     return ret
 
+
+# координаты региона
 Lon0 = 131.9
 Lat0 = 43.1
 Radius = 4.0
 
+# имена баз данных для заданного региона
 DB = 'DS-%5.1f-%4.1f-%3.1f.h5'%(Lon0, Lat0, Radius)
 O3StatFileName = 'O3-%5.1f-%4.1f-%3.1f.pdf'%(Lon0, Lat0, Radius)
+
 
 O3 = pds.read_hdf(DB,'O3')
 O3_Err = pds.read_hdf(DB,'O3Err')
 TH = pds.read_hdf(DB,'TH')
 
-
 # вычисляем сезонную стстистику относительно тропопаузы
-
+# группируем профили озона по сезонам
 O3seasons = O3.groupby(seasons)
-O3seasons_c = O3.groupby(seasons)
+
+# то же делаем для ошибки восстанвления
 O3seasons_Err = O3_Err.groupby(seasons).mean().T / 100.00
+
+# и для высоты тропопаузы
 THseasons = TH.groupby(seasons)
 
+# фирмируем отсчеты высот - относительно замли
 X = np.linspace(0.5, 70, 140) # actual altitudes
+
+# длина нового веткора отсчетов отностительно тропопаузы 
+nXLen = 81
+# и относительно тропопаузы
 nX= np.linspace(-5,35,81) # new altitudes
 
+# Число столбцов в результирующей матрице
+Ncols = 4*2+1
 
-o3mean = pds.DataFrame(index=nX, columns=['Winter','Spring','Summer','Fall'])
-o3mean_err = pds.DataFrame(index=nX, columns=['Winter','Spring','Summer','Fall'])
+# результирующие данные
+# 2D таблицы
+# 1 столбец   - высота
+# 2-5 столбцы - средние профили ВРО для Зимы, Весны, Лета и Осени
+# 6-9 столбцы - средние профили ошибки восстановления ВРО для Зимы, Весны, Лета
+#               и Осени 
+# профили представлены относительно полощения тропопаузы
+OutDS = np.zeros((nXLen, Ncols), dtype=np.float32)
 
-for iseason in ['Winter','Spring','Summer','Fall']:
-    idx = THseasons.groups[iseason]
+
+
+#o3mean = pds.DataFrame(index=nX, columns=['Winter','Spring','Summer','Fall'])
+#o3mean_err = pds.DataFrame(index=nX, columns=['Winter','Spring','Summer','Fall'])
+
+SEASONS = ['Winter','Spring','Summer','Fall']
+# перебираем сезоны и выполняем коррекцию профилей озона на высоту тропопаузы
+for iseason in range(len(SEASONS)):
+    # возьмем индексы профилей для текущего сезона
+    idx = THseasons.groups[SEASONS[iseason]]
+    # и выделим профили измерений
     O3tmp = O3.ix[idx]
     O3Err = O3_Err.ix[idx]
     THtmp = TH.ix[idx]
+    
+    # определим буфферные объекты для хранения временных значений концентрации 
+    # озона, ошибки его измерений
     no3tmp = pds.DataFrame(index=nX,columns=O3tmp.index)
     no3errtmp = pds.DataFrame(index=nX,columns=O3tmp.index)
-    print(len(O3tmp),THtmp.values[0,0])
+
+    # в этом цикле осуществляется привязка профиля ВРО к новой сетке
     for i in range(len(THtmp)):
+        # tmpX - вектор отсчетов высоты относительно тропопаузы
         tmpX = X-THtmp.values[i]
         tmpY = O3tmp.values[i]
+
+        # поскольку профиль концентрации содержит NaN значения, то перед интерполяцией
+        # необходимо найти их положение
         notnan = ~np.isnan(tmpY)
+
+        # интерполируем профиль озона на регулярную сетку относительно высоты тропопаузы
         no3tmp[idx[i]] = np.interp(nX, tmpX[notnan], tmpY[notnan],left=np.nan, right=np.nan)
-        
+
+        # повторяем аналогичные действия для ошибки восстановления профилей концентрации озона
         tmpY = O3Err.values[i]
         notnan = ~np.isnan(tmpY)
+        
         no3errtmp[idx[i]] = np.interp(nX, tmpX[notnan], tmpY[notnan],left=np.nan, right=np.nan)
-#        print(no3tmp[idx[i]])
-    o3mean[iseason] = no3tmp.T.mean()
-    o3mean_err[iseason] = no3tmp.T.std()
-    plt.plot(nX, o3mean[iseason]/1e12, nX, o3mean_err[iseason]/1e12)
-plt.legend(['Winter','Spring','Summer','Fall'])
+
+    OutDS[:,iseason+1] = no3tmp.T.mean()
+    OutDS[:,iseason+5] = no3tmp.T.std()
+    plt.plot(nX, OutDS[:,iseason+1]/1e12, nX, OutDS[:,iseason+5]/1e12)
+
+plt.legend(SEASONS)
 plt.show()
 sys.exit()
 
